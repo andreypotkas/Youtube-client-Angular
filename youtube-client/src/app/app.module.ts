ed6cf6bf0c8708cd3a41087c755504a67ed37c73
andreypotkas <potkasa@gmail.com>
2022-03-29 14:42:22 +0300

    init: start second task (29.03.2022)

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -33,6 +33,7 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- youtube-client/src/app/material/material.module.ts
+++ youtube-client/src/app/material/material.module.ts
@@ -2,22 +2,26 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 export const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
+  MatInputModule,
 ];
 @NgModule({
   imports: [
     MatButtonModule,
     MatButtonToggleModule,
     MatIconModule,
+    MatInputModule,
   ],
   exports: [
     MatButtonModule,
     MatButtonToggleModule,
     MatIconModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule { }
